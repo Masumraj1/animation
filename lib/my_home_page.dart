7c6b0f89a7d1372e5125b895301337b812c5e142
masumraj1 <promotionbd321@gmail.com>
2023-12-21 00:46:41 +0600

    first commit

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -7,34 +7,87 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Animation'),centerTitle: true,),
-      body: Center(
+      appBar: AppBar(
+        title: Text('Animation'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
         child: Column(
           children: [
             GestureDetector(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => MyDetailsPage(tagname: 'sadiTag',imageName: 'assets/sadia.jpg',)));
-              },
-                child: Hero(tag: 'sadiTag',
-                child: Image.asset('assets/sadia.jpg',height: 200,width: 200,))),
-            SizedBox(
-              height: 20,
-            ),
-            GestureDetector(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>MyDetailsPage(tagname: 'masumTag',imageName: 'assets/masum.jpg',)));
-              },
-                child: Hero(tag: 'masumTag',
-                child: Image.asset('assets/masum.jpg'))),
-            SizedBox(
-              height: 20,
-            ),
-            GestureDetector(
-                onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=>MyDetailsPage(tagname: 'babulTag',imageName: 'assets/babul.jpg',)));
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => MyDetailsPage(
+                                tagname: 'sadiTag',
+                                imageName: 'assets/sadia.jpg',
+                              )));
                 },
-                child: Hero(tag: 'babulTag',
-                    child: Image.asset('assets/babul.jpg',height: 200,width: 200,))),
+                child: Hero(
+                    tag: 'sadiTag',
+                    child: Image.asset(
+                      'assets/sadia.jpg',
+                      height: 200,
+                      width: 200,
+                    ))),
+            SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => MyDetailsPage(
+                                tagname: 'masumTag',
+                                imageName: 'assets/masum.jpg',
+                              )));
+                },
+                child: Hero(
+                    tag: 'masumTag', child: Image.asset('assets/masum.jpg'))),
+            SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => MyDetailsPage(
+                                tagname: 'babulTag',
+                                imageName: 'assets/babul.jpg',
+                              )));
+                },
+                child: Hero(
+                    tag: 'babulTag',
+                    child: Image.asset(
+                      'assets/babul.jpg',
+                      height: 200,
+                      width: 200,
+                    ))),
+
+
+             SizedBox(
+               height: 100,
+             ),
+            GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => MyDetailsPage(
+                            tagname: 'babulTag',
+                            imageName: 'assets/babul.jpg',
+                          )));
+                },
+                child: Hero(
+                    tag: 'babulTag',
+                    child: Image.asset(
+                      'assets/babul.jpg',
+                      height: 200,
+                      width: 200,
+                    ))),
           ],
         ),
       ),
